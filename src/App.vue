e13af64546fe54c1a631eacab495244bfdf48d55
SedaII <i.messnaoui@gmail.com>
2021-04-29 16:41:12 +0200

    feat(App): created my first component
    This component does nothing...

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FirstComponent msg ="Voici mon premier component avec Vue" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FirstComponent from './components/FirstComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FirstComponent
   }
 }
 </script>
